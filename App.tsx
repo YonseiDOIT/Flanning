414e5d409935d8de3bfbfdb5ae30e62235059efd
myeongcode <woomg427@yonsei.ac.kr>
2025-01-31 23:57:29 +0900

    Merge v2

--- App.tsx
+++ App.tsx
@@ -5,12 +5,6 @@ import AppProvider from 'src/context/AppProvider';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import AppNavigator from 'src/navigation/AppNavigator';
 import 'react-native-get-random-values';
-import SignupScreen from './src/screens/auth/Signup/SignupScreen';
-import FriendListScreen from 'src/screens/home/friend/FriendListScreen';
-import FriendAddScreen from 'src/screens/home/friend/FriendAddScreen';
-import PlanMakeScreen from 'src/screens/planList/planMake/PlanMakeScreen';
-
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   useEffect(() => {
